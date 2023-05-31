4c00c874e89db9e1411f7593651160631fab3fcc
leesoonwoo <lswteen@gmail.com>
2023-05-31 09:45:53 +0900

    [main] modify : 테스트코드 및 낙관적락 적용진행중

--- domain/src/main/java/kr/co/_29cm/homework/domain/repository/ProductRepository.java
+++ domain/src/main/java/kr/co/_29cm/homework/domain/repository/ProductRepository.java
@@ -4,6 +4,5 @@ import kr.co._29cm.homework.domain.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 }
