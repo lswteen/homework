cd07ed904c1a80aa04c5ea7788b4b86b44621ff3
leesoonwoo <lswteen@gmail.com>
2023-06-02 11:13:28 +0900

    [main] modify : 테스트코드 주석수정

--- service/src/test/java/kr/co/_29cm/homework/OptimisticLockingTest.java
+++ service/src/test/java/kr/co/_29cm/homework/OptimisticLockingTest.java
@@ -66,18 +66,16 @@ public class OptimisticLockingTest {
      * 한번에 3개의 상품재고를 소진하기때문에 동시에 10개를 진행하면 3개만성공하고
      * 7번의 SoldOutException 이 발생하고 filedCount 를 증가 시키게 됩니다.
      *
-     * 낙관적락으로 Entity에 version을 넣고 10번돌리니
-     * ObjectOptimisticLockingFailureException 9번의 카운트가 잡힌것도 확인 하였습니다.
-     * 낙관적락의 경우 첫번째 데이터가 조회하고 0버전을 업데이트 1로 하고 나머지 9번스레드는 전부실패되어
-     *
      * 요구사항에서 멀티스레드 동시 재고차감에서 SoldOutException 이 목적이기에 해당 Rock을
      * LockModeType.PESSIMISTIC_WRITE 으로 변경 하였습니다.
      *
-     *  처음 테이블구조는 product 에 상품번호, 상품명, 가격, 상품재고 같이 있는 구조였지만
-     *  LockModeType.PESSIMISTIC_WRITE 으로 인하여 상품 테이블 트랜잭션락으로 인하여
+     * 낙관적으로도도
      *
-     *  추후 데이터가 증가하게되면 상품조회 와 재고 변경 이라는 Read/Write
-     *  사용빈도가 다른 2개의 테이블을 용도에 맞게 분리 하였습니다.
+     * 처음 테이블구조는 product 에 상품번호, 상품명, 가격, 상품재고 같이 있는 구조였지만
+     * LockModeType.PESSIMISTIC_WRITE 으로 인하여 상품 테이블 트랜잭션락으로 인하여
+     *
+     * 추후 데이터가 증가하게되면 상품조회 와 재고 변경 이라는 Read/Write
+     * 사용빈도가 다른 2개의 테이블을 용도에 맞게 분리 하였습니다.
      *
      * @throws ExecutionException
      * @throws InterruptedException
