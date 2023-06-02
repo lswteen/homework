769cb387c7e8614b862aa39269e00f81691c3695
leesoonwoo <lswteen@gmail.com>
2023-06-02 16:23:57 +0900

    [main] modify : 리펙토링

--- service/src/main/java/kr/co/_29cm/homework/shell/prompt/OrderAction.java
+++
@@ -1,8 +0,0 @@
-package kr.co._29cm.homework.shell.prompt;
-
-import org.jline.reader.LineReader;
-
-@FunctionalInterface
-public interface OrderAction {
-    void execute(LineReader lineReader);
-}
